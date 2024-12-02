9d6c7ce118ab5f090d5ac008422b09b0fb48adae
Brianna Babiack <bbabiack@gmail.com>
2024-12-02 09:07:35 -0500

    Day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,10 @@
 use std::fs;
 
+struct Similarity{
+    list_a_number: i32,
+    list_b_count: i32
+}
+
 fn main() {
     let file_path = "input.txt";
     //let file_path = "test_case.txt";
@@ -8,7 +13,10 @@ fn main() {
     let mut list_b = Vec::new();
     let mut list_c = Vec::new();
 
+    let mut similarity_scores: Vec<Similarity> = Vec::new();
+
     let mut total_distance = 0;
+    let mut total_similarity_score = 0;
     for line in data.lines() {
         println!("{}", line);
        let temp_line: Vec<i32> = line.split(' ').filter(|str| !str.is_empty()).map(|num| num.trim().parse::<i32>().unwrap()).collect();
@@ -19,6 +27,8 @@ fn main() {
        list_a.push(temp_line[0]);
        list_b.push(temp_line[1]);
     }
+
+    // Part 1
     list_a.sort();
     list_b.sort();
     for i in 0..list_a.len() { 
@@ -33,9 +43,22 @@ fn main() {
     for num in list_c {
         total_distance += num;
     }
+    println!("Total distance {}", total_distance);
 
 
-    println!("Total distance {}", total_distance)
+
+    // Part 2
+    for num in list_a {
+        let count = list_b.iter().filter(|&&numb| numb == num).count() as i32;
+        similarity_scores.push(Similarity {list_a_number: num, list_b_count: count});
+    }
+
+    for score in similarity_scores{
+        total_similarity_score += score.list_a_number * score.list_b_count
+    }
+    println!("Total Similarity Score {}", total_similarity_score);
+
+
     
 }
 
