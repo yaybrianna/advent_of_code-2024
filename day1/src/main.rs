29211245101fec28ab47c2e309d7b4107fadfae7
Brianna Babiack <bbabiack@gmail.com>
2024-12-01 23:22:05 -0500

    update files for 2024 and solve day 1 part 1

---
+++ day1/src/main.rs
@@ -0,0 +1,45 @@
+use std::fs;
+
+fn main() {
+    let file_path = "input.txt";
+    //let file_path = "test_case.txt";
+    let data = Box::new(load_input(&file_path));
+    let mut list_a = Vec::new();
+    let mut list_b = Vec::new();
+    let mut list_c = Vec::new();
+
+    let mut total_distance = 0;
+    for line in data.lines() {
+        println!("{}", line);
+       let temp_line: Vec<i32> = line.split(' ').filter(|str| !str.is_empty()).map(|num| num.trim().parse::<i32>().unwrap()).collect();
+       println!("{:?}", temp_line);
+       if temp_line.len() > 4 {
+        println!("Something went wrong and there are too many substrings per line...");
+       }
+       list_a.push(temp_line[0]);
+       list_b.push(temp_line[1]);
+    }
+    list_a.sort();
+    list_b.sort();
+    for i in 0..list_a.len() { 
+        if list_a[i] > list_b[i] {
+            list_c.push(list_a[i] - list_b[i]);
+        }
+        else {
+            list_c.push(list_b[i] - list_a[i]);
+        }
+    }
+
+    for num in list_c {
+        total_distance += num;
+    }
+
+
+    println!("Total distance {}", total_distance)
+    
+}
+
+fn load_input(file_path: &str) -> String {
+    return fs::read_to_string(file_path).expect("Unable to read file");
+}
+
