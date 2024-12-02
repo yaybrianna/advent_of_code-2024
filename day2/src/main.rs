4bfd48ee8c17196c446097ca0250980d6b6cc70f
Brianna Babiack <BBabiack@ebsco.com>
2024-12-02 12:28:01 -0500

    day2 part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,14 +1,15 @@
-use std::fs;
+use std::{fs, env};
 
 const MINIMUM_VARIATION: i32 = 1;
 const MAXIMUM_VARIATION: i32 = 3;
 
 fn main() {
-    // let file_path = "input.txt";
-    let file_path = "test_case.txt";
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    println!("Using file: {}", file_path);
     let raw_data = Box::new(load_input(&file_path));
     let mut data = Vec::new();
-
+    let mut failed_report_indexes = Vec::new();
     // parse input file into 2D vec
     for line in raw_data.lines() {
         let temp_line: Vec<i32> = line
@@ -21,46 +22,67 @@ fn main() {
 
     let mut safe_report_count = 0;
     // Part 1
-    for report in &data {
-        println!("{:?}", report);
-        let mut is_safe_report = false;
-        let is_increasing = report[0] < report[1];
-        let mut failing_level_count = 0;
-        for i in 0..report.len() - 1 {
-            if is_increasing && report[i] > report[i + 1] {
-                println!("We changed directions, unsafe report!");
-                is_safe_report = false;
-                failing_level_count += 1;
-                break;
-            }
-            let difference = if is_increasing {
-                report[i + 1] - report[i]
-            } else {
-                report[i] - report[i + 1]
-            };
-            if difference < MINIMUM_VARIATION || difference > MAXIMUM_VARIATION {
-                println!(
-                    "The difference between index {} and index {}: {} is outside the allowed range, unsafe report",
-                    i,
-                    i + 1,
-                    difference
-                );
-                is_safe_report = false;
-                failing_level_count += 1;
-                break;
-            }
-            is_safe_report = true;
-        }
-        println!("Failing level count: {}", failing_level_count);
-        if is_safe_report {
+    for i in 0..data.len() {
+        println!("{:?}", &data[i]);
+        let is_safe = is_safe_report(&data[i]);
+        if is_safe {
             safe_report_count += 1;
+        } else {
+            failed_report_indexes.push(i);
         }
     }
 
     println!("Total Safe Reports: {}/{}", safe_report_count, &data.len());
     // Part 2
+
+    println!("\nFailed Report Numbers: {:?}\n", failed_report_indexes);
+    println!("INITIALIZING PROBLEM DAMPENER (tm)...\n");
+    for report_index in failed_report_indexes {
+        for i in 0..data[report_index].len() {
+            let mut short_report = data[report_index].clone();
+            short_report.remove(i);
+            let is_safe = is_safe_report(&short_report);
+            if is_safe {
+                safe_report_count += 1;
+                break;
+            }
+        }
+    }
+    println!(
+        "Total Safe Reports with Problem Dampener(tm): {}/{}",
+        safe_report_count,
+        &data.len()
+    );
 }
 
 fn load_input(file_path: &str) -> String {
     return fs::read_to_string(file_path).expect("Unable to read file");
+}
+
+fn is_safe_report(report: &Vec<i32>) -> bool {
+    println!("{:?}", report);
+    let starts_increasing = report[0] < report[1];
+    for i in 0..report.len() - 1 {
+        if (starts_increasing && report[i] > report[i + 1])
+            || (!starts_increasing && report[i + 1] > report[i])
+        {
+            println!("We changed directions, unsafe report!");
+            return false;
+        }
+        let difference = if starts_increasing {
+            report[i + 1] - report[i]
+        } else {
+            report[i] - report[i + 1]
+        };
+        if difference < MINIMUM_VARIATION || difference > MAXIMUM_VARIATION {
+            println!(
+                    "The difference between index {} and index {}: {} is outside the allowed range, unsafe report",
+                    i,
+                    i + 1,
+                    difference
+                );
+            return false;
+        }
+    }
+    return true;
 }
